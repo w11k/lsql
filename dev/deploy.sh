0f1639b45a40557645a57a4a09af1fb6447d44bf
Roman Roelofsen <romanroe@gmail.com>
2013-07-17 16:33:50 +0200

    wip

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -6,16 +6,16 @@ function exit_on_error {
 
 cd `dirname $0`/..
 
+../mvnrepository/check_repository_status.sh
+exit_on_error
+
 changes=$(git status --porcelain 2>/dev/null| egrep "^(AM|M| M|\?\?)" | wc -l)
 if [[ $changes != 0 ]]; then
-    echo "Repository contains pending changes."
+    echo "Project contains pending changes."
     exit 1
 fi
 
 dev/test_example.sh
-exit_on_error
-
-../mvnrepository/check_repository_status.sh
 exit_on_error
 
 mvn -Dmaven.test.skip=true -DaltDeploymentRepository=repo-snapshots::default::file:../mvnrepository/snapshots deploy
