7f144e7df5a71bbf9eb662ec5b3a96029cf65c08
Roman Roelofsen <romanroe@gmail.com>
2019-03-05 09:53:56 +0100

    - moved CLI test classes outside test package

---
+++ lsql-cli-tests/src/generated/java/com/w11k/lsql/cli/structural_fields/Field_Com_W11k_Lsql_Cli_Testcliconfig_Customtype.java
@@ -0,0 +1,9 @@
+package com.w11k.lsql.cli.structural_fields;
+
+public interface Field_Com_W11k_Lsql_Cli_Testcliconfig_Customtype {
+
+    com.w11k.lsql.cli.TestCliConfig.CustomType getField();
+
+    Field_Com_W11k_Lsql_Cli_Testcliconfig_Customtype withField(com.w11k.lsql.cli.TestCliConfig.CustomType val);
+
+}
